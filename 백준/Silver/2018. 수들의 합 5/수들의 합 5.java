45f5442f4091e29b287b056562508d362f5fae03
goozip2 <92193151+goozip2@users.noreply.github.com>
2024-07-04 22:52:41 +0900

    [Silver V] Title: 수들의 합 5, Time: 172 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -3,25 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		int count = 0;
-		for(int i=N;i>0;i--) {
-			int sum = 0;
-			for(int j=i;j>0;j--) {
-				sum+=j;
-				if(sum == N) {
-					count++;
-					break;
-				} else if(sum > N) {
-					break;
-				}
+		int count = 1; //N 포함된 상태
+		int start_index = 1;
+		int end_index = 1;
+		int sum = 1;
+		while(end_index!=N) {
+			if(sum == N) {
+				count++;
+				end_index++;
+				sum += end_index;
+			} else if(sum>N) {
+				sum -= start_index;
+				start_index++;
+			} else if(sum<N) {
+				end_index++;
+				sum += end_index;
 			}
 		}
 		System.out.print(count);
 	}
-
 }
