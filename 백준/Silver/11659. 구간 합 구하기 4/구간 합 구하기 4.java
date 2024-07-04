233da98c9ea9cd5f44c536ff1e455ccf9423bc0c
goozip2 <92193151+goozip2@users.noreply.github.com>
2024-07-04 16:34:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1540 ms, Memory: 58232 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] sum = new int[N+1];
+		sum[0] = 0;
+		for(int i=1;i<=N;i++) {
+			sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			System.out.println(sum[end]-sum[start-1]);
+			
+		}
+		
+
+	}
+
+}
