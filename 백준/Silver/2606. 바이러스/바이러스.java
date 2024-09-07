b673a004444d183936a7f0c6264bc9e0f5c4bb0a
goozip2 <92193151+goozip2@users.noreply.github.com>
2024-09-07 17:30:59 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -7,44 +7,54 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	static int C;
+	static int R;
+	
 	static int[][] network;
 	static boolean[] visited;
-	static int virusComCount = 0;
-	static int comTotal;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		comTotal = Integer.parseInt(br.readLine());
-		network = new int[comTotal][comTotal];
-		visited = new boolean[comTotal];
-		int networkTotal = Integer.parseInt(br.readLine());
+		C = Integer.parseInt(br.readLine());
+		R = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st;
-		for(int i=0;i<networkTotal;i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken()) - 1;
-			int e = Integer.parseInt(st.nextToken()) - 1;
+		visited = new boolean[C];
+		network = new int[C][C];
+		
+		for(int r=0;r<R;r++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken())-1;
+			int e = Integer.parseInt(st.nextToken())-1;
 			network[s][e] = 1;
 			network[e][s] = 1;
 		}
 		
-		bfs();
-		System.out.println(virusComCount);
+		int count = bfs();
+		System.out.println(count);
 	}
-	public static void bfs() {
+	
+	static int bfs() {
+		int virusCount = 0;
 		Queue<Integer> queue = new LinkedList<>();
-		visited[0] = true;
 		queue.add(0);
+		visited[0] = true;
+		
 		while(!queue.isEmpty()) {
-			int nowCom = queue.poll();
-			for(int i=0;i<comTotal;i++) {
-				if(visited[i] == false && network[nowCom][i]==1) {
-					virusComCount++;
-					visited[i] = true;
-					queue.add(i);
+			int now = queue.poll();
+			for(int next=0;next<C;next++) {
+				if(network[now][next] == 1) {
+					if(next <0 || next>=C) {
+						continue;
+					}
+					if(visited[next] == true) {
+						continue;
+					}
+					queue.add(next);
+					virusCount++;
+					visited[next] = true;
 				}
 			}
 		}
+		return virusCount;
 	}
-
 }
