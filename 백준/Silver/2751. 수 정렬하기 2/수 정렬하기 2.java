09e376963805bf274aed4efc450225bca7a94df0
goozip2 <92193151+goozip2@users.noreply.github.com>
2024-12-01 23:30:43 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1368 ms, Memory: 132112 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,30 @@
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-
+ 
+ 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+    
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int N = Integer.parseInt(br.readLine());
 		
+		// list 계열 중 하나를 쓰면 된다.
 		ArrayList<Integer> list = new ArrayList<>();
-		for(int i=0;i<N;i++) {
+		
+		for(int i = 0; i < N; i++) {
 			list.add(Integer.parseInt(br.readLine()));
 		}
+		
 		Collections.sort(list);
 		
-		for(int n: list) {
-			sb.append(n).append("\n");
+		for(int value : list) {
+			sb.append(value).append('\n');
 		}
-		System.out.print(sb);
+		System.out.println(sb);
 	}
-
 }
