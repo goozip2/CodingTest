499388a5f196d7c577986361d14b26a62528c147
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-10 13:00:33 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,37 +1,33 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder result = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		int subjectNumber = 0;
-		double[] subjectArr;
-		double maxScore = 0;
-		double total = 0;
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		subjectNumber = Integer.parseInt(br.readLine());
-		
-		subjectArr = new double[subjectNumber];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<subjectNumber;i++) {
-			subjectArr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(subjectArr);
-		maxScore = subjectArr[subjectNumber-1];
-		for(int i=0;i<subjectNumber;i++) {
-			subjectArr[i] = subjectArr[i]/maxScore*100;
-			total += subjectArr[i];
-		}
-		
-		System.out.println(total/subjectNumber);
-	}
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.MIN_VALUE;
 
+        int[] scores = new int[N];
+        String[] sscores = br.readLine().split(" ");
+
+        for(int n=0;n<N;n++) {
+            scores[n] = Integer.parseInt(sscores[n]);
+            if(scores[n] > M) {
+                M = scores[n];
+            }
+        }
+
+        double newSum = 0;
+        for(int score: scores) {
+            newSum += ((double)score/M*100);
+        }
+
+        double newMean = (double)newSum/N;
+        result.append(newMean);
+        System.out.println(newMean);
+    }
 }
