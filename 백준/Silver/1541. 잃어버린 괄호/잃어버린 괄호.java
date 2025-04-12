fe3a0b0d98d7c14a75f9fc63f7a1fc36ea1b6aea
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-12 22:00:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,37 +1,33 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] plusGroups = br.readLine().split("-");
+        int P = plusGroups.length;
+        int[] plusGroupResults = new int[P];
 
-		// (-) 기호 기준으로 나눔
-		String[] subStringArr = br.readLine().split("-");
-		int[] subSumArr = new int[subStringArr.length];
+        for(int i=0;i<P;i++) {
+            String plusGroup = plusGroups[i];
+            String[] nums = plusGroup.split("\\+");
+            int sum = 0;
+            for(String s: nums){
+                sum += Integer.parseInt(s);
+            }
+            plusGroupResults[i] = sum;
+        }
+        int result = plusGroupResults[0];
+        for(int i=1;i<P;i++) {
+            result -= plusGroupResults[i];
+        }
 
-		// subString(덧셈으로 이루어진 부분) 각각 계산
-		for (int i = 0; i < subStringArr.length; i++) {
-			String[] Snumbers = subStringArr[i].split("\\+");
-			int[] Inumbers = new int[Snumbers.length];
-			// String을 int화
-			for (int j = 0; j < Snumbers.length; j++) {
-				Inumbers[j] = Integer.parseInt(Snumbers[j]);
-			}
-			int subSum = 0;
-			for (int n : Inumbers) {
-				subSum += n;
-			}
-			subSumArr[i] = subSum;
-		}
+        System.out.println(result);
 
-		int result = subSumArr[0];
-		for (int x = 1; x < subSumArr.length; x++) {
-			result -= subSumArr[x];
-		}
-		System.out.print(result);
-	}
-
+    }
 }
+
