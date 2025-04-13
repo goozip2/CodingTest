d5afcab01214f99449f7bcd98c2da4ad6bdafbe5
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-13 10:54:26 +0900

    [Silver IV] Title: 로프, Time: 372 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,8 +1,12 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,12 +20,11 @@ public class Main {
         }
         Arrays.sort(ropeArr);
 
-        int[] possibleWeightArr = new int[N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int n=0;n<N;n++) {
-            possibleWeightArr[n] = ropeArr[n]*(N-n);
+            pq.offer(ropeArr[n]*(N-n));
         }
-        Arrays.sort(possibleWeightArr);
-        
-        System.out.println(possibleWeightArr[N-1]);
+
+        System.out.println(pq.poll());
     }
 }
