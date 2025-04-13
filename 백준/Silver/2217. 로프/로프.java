5a90da812edc938cffccea577b5d861a7b707b55
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-13 10:52:46 +0900

    [Silver IV] Title: 로프, Time: 408 ms, Memory: 27452 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,29 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
+        int[] ropeArr = new int[N];
+        for(int n=0;n<N;n++) {
+            int currentRope = Integer.parseInt(br.readLine());
+            ropeArr[n] = currentRope;
+        }
+        Arrays.sort(ropeArr);
 
-		Integer[] lope = new Integer[N];
-		int[] result = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			lope[i] = scan.nextInt();
-		}
-
-		Arrays.sort(lope, Collections.reverseOrder());
-
-		for (int i = 0; i < N; i++) {
-			result[i] = lope[i] * (i + 1);
-		}
-
-		Arrays.sort(result);
-		System.out.print(result[N - 1]);
-	}
-
+        int[] possibleWeightArr = new int[N];
+        for(int n=0;n<N;n++) {
+            possibleWeightArr[n] = ropeArr[n]*(N-n);
+        }
+        Arrays.sort(possibleWeightArr);
+        
+        System.out.println(possibleWeightArr[N-1]);
+    }
 }
