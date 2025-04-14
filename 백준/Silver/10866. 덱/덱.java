ec4614398cac58d13d62158dea4d406e541f067d
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-15 08:43:01 +0900

    [Silver IV] Title: 덱, Time: 168 ms, Memory: 18704 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,69 +1,69 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		Deque<Integer> deque = new LinkedList<>();
-
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String S = st.nextToken();
-
-			switch (S) {
-			case "push_front":
-				deque.addFirst(Integer.parseInt(st.nextToken()));
-				break;
-			case "push_back":
-				deque.addLast(Integer.parseInt(st.nextToken()));
-				break;
-			case "pop_front":
-				if (deque.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(deque.removeFirst());
-				}
-				break;
-			case "pop_back":
-				if (deque.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(deque.removeLast());
-				}
-				break;
-			case "size":
-				System.out.println(deque.size());
-				break;
-			case "empty":
-				if (deque.isEmpty()) {
-					System.out.println(1);
-				} else {
-					System.out.println(0);
-				}
-				break;
-			case "front":
-				if (deque.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(deque.getFirst());
-				}
-				break;
-			case "back":
-				if (deque.isEmpty()) {
-					System.out.println(-1);
-				} else {
-					System.out.println(deque.getLast());
-				}
-				break;
-			}
-
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> deque = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int n=0;n<N;n++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int x;
+            switch (command) {
+                case("push_front"):
+                    x = Integer.parseInt(st.nextToken());
+                    deque.offerFirst(x);
+                    break;
+                case("push_back"):
+                    x = Integer.parseInt(st.nextToken());
+                    deque.offerLast(x);
+                    break;
+                case("pop_front"):
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollFirst()).append("\n");
+                    }
+                    break;
+                case("pop_back"):
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.pollLast()).append("\n");
+                    }
+                    break;
+                case("size"):
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case("empty"):
+                    sb.append(deque.isEmpty()?1:0).append("\n");
+                    break;
+                case("front"):
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.getFirst()).append("\n");
+                    }
+                    break;
+                case("back"):
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(deque.getLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
 }
