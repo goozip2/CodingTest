160da6b65311bce324138c065184c73b28de3f21
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-15 08:32:46 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,59 +1,54 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		// 명령어 개수 입력
-		int N = Integer.parseInt(br.readLine());
-		Queue<Integer> queue = new LinkedList<Integer>();
-		int last = 0;
-		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String S = st.nextToken();
-			
-
-			switch (S) {
-			case "push":
-				last = Integer.parseInt(st.nextToken());
-				queue.add(last);
-				break;
-			case "pop":
-				if (queue.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(queue.remove()).append("\n");
-				break;
-			case "size":
-				sb.append(queue.size()).append("\n");
-				break;
-			case "empty":
-				if (queue.isEmpty())
-					sb.append(1).append("\n");
-				else
-					sb.append(0).append("\n");
-				break;
-			case "front":
-				if (queue.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(queue.peek()).append("\n");
-				break;
-			case "back":
-				if (queue.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(last).append("\n");
-				break;
-			}
-		}
-		System.out.println(sb);
-
-	}
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> queue = new ArrayDeque<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for(int n=0;n<N;n++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch(command) {
+                case("push"):
+                    int x = Integer.parseInt(st.nextToken());
+                    queue.offerLast(x);
+                    break;
+                case("pop"):
+                    if(queue.isEmpty()) {
+                        sb.append("-1").append("\n");
+                    } else {
+                        sb.append(queue.pollFirst()).append("\n");
+                    }
+                    break;
+                case("size"):
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case("empty"):
+                    sb.append(queue.isEmpty()?1:0).append("\n");
+                    break;
+                case("front"):
+                    if(queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peek()).append("\n");
+                    }
+                    break;
+                case("back"):
+                    if(queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peekLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
 }
