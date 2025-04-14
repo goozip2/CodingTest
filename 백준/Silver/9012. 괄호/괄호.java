3571065f73302710b0ff9b1a9dc3e3c6fe7c9afe
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-15 08:58:07 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,41 +1,41 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<N;i++) {
-			Stack<Character> stack = new Stack<Character>();
-			int result = 1;
-			
-			String s = br.readLine();
-			for(int j=0;j<s.length();j++) {
-				char c = s.charAt(j);
-				if(c == '(') {
-					stack.push(c);
-				} else if(c == ')') {
-					if(!stack.isEmpty() && stack.peek() == '(') {
-						stack.pop();
-					} else {
-						result = 0;
-						break;
-					}
-				}
-			}
-			if(result == 1 && stack.isEmpty()) {
-				sb.append("YES\n");
-			} else {
-				sb.append("NO\n");
-			}
-		}
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int t=0;t<T;t++) {
+            Deque<Character> stack = new ArrayDeque<>();
+            char[] input = br.readLine().toCharArray();
+            boolean VPS = true;
+            for(char c: input) {
+                switch (c) {
+                    case('('):
+                        stack.push('(');
+                        break;
+                    case(')'):
+                        if(stack.isEmpty()) {
+                            VPS = false;
+                        } else if (stack.peek()!='('){
+                            VPS = false;
+                        } else {
+                            stack.pop();
+                        }
+                        break;
+                }
+            }
+            if(VPS && stack.isEmpty()) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
