6a9ec98022c9f24a2551306a8332cc8a0a363a08
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-15 09:18:43 +0900

    [Silver II] Title: 스택 수열, Time: 316 ms, Memory: 28620 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,40 +1,55 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.regex.Pattern;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> stack = new ArrayDeque<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] targetArr = new int[N];
+        int targetIndex = 0;
+        for(int i=0;i<N;i++) {
+            targetArr[i] = Integer.parseInt(br.readLine());
+        }
+        boolean result = true;
+        for(int n=1;n<=N;) {
+            if(stack.isEmpty()) {
+                stack.push(n);
+                sb.append('+').append("\n");
+                n++;
+            } else if (stack.peek()<targetArr[targetIndex]) {
+                stack.push(n);
+                sb.append('+').append("\n");
+                n++;
+            } else if(stack.peek()==targetArr[targetIndex]) {
+                stack.pop();
+                sb.append('-').append("\n");
+                targetIndex++;
+            } else if(stack.peek()>targetArr[targetIndex]) {
+                result = false;
+                break;
+            }
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
+        }
+        while(!stack.isEmpty()){
+            if(stack.peek()==targetArr[targetIndex]) {
+                stack.pop();
+                sb.append('-').append("\n");
+                targetIndex++;
+            } else if(stack.peek()>targetArr[targetIndex]) {
+                result = false;
+                break;
+            }
+        }
 
-		int top = 0;
-		for (int i = 0; i < N; i++) {
-			int n = Integer.parseInt(br.readLine());
-			// n을 pop할 수 있도록, n까지 넣을 수 있게 stack에 push
-			if (n > top) {
-				for (int j = top + 1; j <= n; j++) {
-					stack.push(j);
-					sb.append("+\n");
-				}
-				top = n;
-			}
-			if (stack.get(stack.size() - 1) == n) {
-				stack.pop();
-				sb.append("-\n");
-			}
-		}
+        System.out.println(result?sb:"NO");
 
-		// 스택이 안비어있으면
-		if (stack.size() > 0) {
-			System.out.println("NO");
-		} else {
-			System.out.println(sb);
-		}
-	}
-
+    }
 }
