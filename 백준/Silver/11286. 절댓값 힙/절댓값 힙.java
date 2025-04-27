62cf8b5762110c47faa6512d378de91584cff350
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-28 07:31:35 +0900

    [Silver I] Title: 절댓값 힙, Time: 272 ms, Memory: 25456 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,36 +7,35 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				//절대값 기준으로 앞 값이 더 크다면 자리를 바꿔준다.
-				if(Math.abs(o1) > Math.abs(o2)) {
-					return Math.abs(o1) - Math.abs(o2);
-				//절대값 기준으로 두 값이 같다면 음수를 앞으로 보내준다.
-				}else if(Math.abs(o1) == Math.abs(o2)) {
-					return o1 - o2;
-				}else {
-					return -1;
-				}
-			}
-		});
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < N; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if(x == 0) {
-				if(pq.isEmpty()) sb.append("0").append("\n");
-				else sb.append(pq.poll()).append("\n");
-			}else {
-				pq.offer(x);
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // 절댓값이 작은 게 우선
+                // 절댓값 중에서도 실제값이 작은 게 우선
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }
+                return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+
+        int N = Integer.parseInt(br.readLine());
+        for (int n = 0; n < N; n++) {
+            int in = Integer.parseInt(br.readLine());
+            if (in == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(in);
+            }
+        }
+        System.out.println(sb);
+    }
 }
