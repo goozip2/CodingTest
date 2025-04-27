da8f2293f02ea570c9f1207cf3b52742a95022bb
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-28 07:39:13 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 26208 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int n = 0; n < N; n++) {
+            int in = Integer.parseInt(br.readLine());
+            if (in == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(in);
+            }
+        }
+        System.out.println(sb);
+    }
+}
