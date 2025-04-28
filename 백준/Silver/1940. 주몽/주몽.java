700468a5b83ef29217b714e9456ff8d49f01cbc0
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-04-29 08:31:55 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,38 +6,34 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        int result = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int n=0;n<N;n++) {
+            arr[n] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		int start = 0;
-		int end = N-1;
-		
-		int sum = 0;
-		int result = 0;
-		while(start!=end) {
-			sum = arr[start] + arr[end];
-			if(sum == M) {
-				result++;
-				start++;
-			} else if (sum<M) {
-				start++;
-			} else if (sum>M) {
-				end--;
-			}
-		}
-		
-		System.out.println(result);
-	}
+        int left = 0;
+        int right = N-1;
+        int sum = 0;
 
+        while(left<right) {
+            sum = arr[left] + arr[right];
+            if(sum > M) {
+                right--;
+            } else if(sum<M) {
+                left++;
+            } else {
+                result++;
+                left++;
+                right--;
+            }
+        }
+        System.out.println(result);
+    }
 }
