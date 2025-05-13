8f78aa0af629d47b2159dc63699e6b3b79669359
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-05-13 09:09:22 +0900

    [Silver II] Title: DNA 비밀번호, Time: 264 ms, Memory: 21968 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,73 +1,82 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] check;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int S = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-		
-		String str = br.readLine();
-		char[] pw = new char[S];
-		int[] dnaNum = new int[4];
-		int[] currNum = new int[4];
-		check = new int[4];
-		int result=0;
-		
-		for(int i=0;i<S;i++) { //몇 개 있는지 체크
-			pw[i] = str.charAt(i);
-			if(pw[i] == 'A') dnaNum[0]++;
-			if(pw[i]=='C') dnaNum[1]++;
-			if(pw[i]=='G') dnaNum[2]++;
-			if(pw[i]=='T') dnaNum[3]++;
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<4;i++) {//최소로 들어가야하는 개수
-			check[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		if(!checkNum(dnaNum)) {
-			System.out.print(result);
-			return;
-		}
-		
-		for(int i=0;i<P;i++) { //슬라이딩하기 위한 초기화 과정
-			if(pw[i] == 'A') currNum[0]++;
-			if(pw[i]=='C') currNum[1]++;
-			if(pw[i]=='G') currNum[2]++;
-			if(pw[i]=='T') currNum[3]++;
-		}
-		if(checkNum(currNum)) result++;
-		
-		for(int j=P;j<S;j++) { //j는 뒷부분 인덱스
-			int i=j-P; //i는 앞부분 인덱스
-			
-			if(pw[i]=='A') currNum[0]--;
-			if(pw[i]=='C') currNum[1]--;
-			if(pw[i]=='G') currNum[2]--;
-			if(pw[i]=='T') currNum[3]--;
-			
-			if(pw[j]=='A') currNum[0]++;
-			if(pw[j]=='C') currNum[1]++;
-			if(pw[j]=='G') currNum[2]++;
-			if(pw[j]=='T') currNum[3]++;
-			
-			if(checkNum(currNum)) result++;
-		}
-		
-		System.out.print(result);
-	}
-	
-	public static boolean checkNum(int[] num) {
-		for(int i=0;i<4;i++) {
-			if(num[i] < check[i])
-				return false;
-		}
-		return true;
-	}
+    public static void main(String[] args) throws IOException {
+        int result = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        char[] dna = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+        int[] tBase = new int[4];
+        tBase[0] = Integer.parseInt(st.nextToken());
+        tBase[1] = Integer.parseInt(st.nextToken());
+        tBase[2] = Integer.parseInt(st.nextToken());
+        tBase[3] = Integer.parseInt(st.nextToken());
+
+        int[] cBase = new int[4];
+        Arrays.fill(cBase, 0);
+
+        for(int i=0;i<P;i++) {
+            add(dna[i], cBase);
+        }
+        if(check(cBase, tBase)) {
+            result++;
+        }
+        for(int i=P;i<S;i++) {
+            add(dna[i], cBase);
+            delete(dna[i-P], cBase);
+            if(check(cBase, tBase)) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static void add(char base, int[] cBase) {
+        switch (base) {
+            case 'A':
+                cBase[0]++;
+                break;
+            case 'C':
+                cBase[1]++;
+                break;
+            case 'G':
+                cBase[2]++;
+                break;
+            case 'T':
+                cBase[3]++;
+                break;
+        }
+    }
+
+    public static void delete(char base, int[] cBase) {
+        switch (base) {
+            case 'A':
+                cBase[0]--;
+                break;
+            case 'C':
+                cBase[1]--;
+                break;
+            case 'G':
+                cBase[2]--;
+                break;
+            case 'T':
+                cBase[3]--;
+                break;
+        }
+    }
+
+    public static boolean check(int[] cBase, int[] tBase) {
+        return cBase[0] >= tBase[0] && cBase[1] >= tBase[1] && cBase[2] >= tBase[2] && cBase[3] >= tBase[3];
+    }
 }
